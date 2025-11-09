1ac8db1b63f2574f5dfd782933b052d0ecb60338
yosifivanov08 <kirmiziagac0@gmail.com>
2025-11-09 17:45:25 +0200

    Added Files

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    static JFrame frame = new JFrame("Musclemass FitnessApp");
+    static double age;
+    static double weight;
+    static double height;
+
+    public static void main(String[] args) {
+        SignUp.createPanel();
+    }
+}
