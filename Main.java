1488939a61d08933647f2bb621713126c7b04b04
yosifivanov08 <kirmiziagac0@gmail.com>
2025-11-09 18:08:19 +0200

    Logo relocated

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-import javax.swing.*;
-
+import java.awt.Color;
+import java.awt.Font;
+import  javax.swing.*;
 public class Main {
     static JFrame frame = new JFrame("Musclemass FitnessApp");
     static double age;
@@ -7,6 +8,13 @@ public class Main {
     static double height;
 
     public static void main(String[] args) {
+        
+        JLabel logo = new JLabel("MuscleMass Fitness App");
+        logo.setBounds(205, 650, 150, 90);
+        logo.setForeground(Color.white);
+        logo.setFont(new Font("Arial", Font.BOLD, 10));
+
         SignUp.createPanel();
+        Interface.showInterface();
     }
 }
